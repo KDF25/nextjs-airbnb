62a861cc5359ad9c8af096545b900423f46e43d8
KDF25 <karimov.damir.faridovich@gmail.com>
2025-03-27 18:13:16 +0500

    feat: add trips page

---
+++ app/trips/page.tsx
@@ -0,0 +1,36 @@
+import { ClientOnly, EmptyState } from "@shared/ui";
+import { TripsClient } from "@widgets/tripsClient";
+import { getCurrentUser, getReservations } from "../actions";
+
+export default async function TripsPage() {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    userId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you havent reserved any trips."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <TripsClient reservations={reservations} currentUser={currentUser} />
+    </ClientOnly>
+  );
+}
