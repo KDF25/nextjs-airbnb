85868a9abd24a3103e583aeda6d043f90cb1b74c
KDF25 <karimov.damir.faridovich@gmail.com>
2025-03-28 01:53:57 +0500

    fix: change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,23 @@ export const metadata = {
   title: "Airbnb",
   description: "Airbnb",
   icons: "https://www.seekpng.com/png/full/957-9571167_airbnb-png.png",
+  openGraph: {
+    title: "Airbnb",
+    description:
+      "Explore a world of unique stays and experiences with Airbnb. Book your next adventure in over 220 countries and regions.",
+    url: process.env.NEXT_BASE_URL,
+    siteName: "Airbnb",
+    images: [
+      {
+        url: "https://a0.muscache.com/im/pictures/fe7217ff-0b24-438d-880d-b94722c75bf5.jpg",
+        width: 800,
+        height: 600,
+        alt: "Airbnb",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
 };
 
 const font = Nunito({
